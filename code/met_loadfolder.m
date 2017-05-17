34498506276172bab324dc0803782fe400bb555e
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-05-17 15:37:56 +0100

    Rename met folder loader and tidy comments

--- code/met_loadfolder.m
+++ code/met_loadfolder.m
@@ -1,17 +1,18 @@
-function met = loadfolder(filespec)
-% loadfolder Load a folder of text files matching filespec
+function met = met_loadfolder(filespec)
+% loadfolder Load text files in the current folder matching filespec
 % Example usage: 
-%     met = loadfolder('met_mlo*.txt');
+%     met = met_loadfolder('met_mlo*.txt');
 
     % Create structure containing info about files.
     dirinfo = dir(filespec);
     nfiles = size(dirinfo,1);
     
-    % Loop through files, calling data loader on each of them.
     % Create an empty structure to load data into
     met = struct;
+    
+    % Loop through files, calling data loader on each of them.
     for i=1:nfiles
         filename=dirinfo(i).name;
-        met = importfile(filename,i,met);
+        met = met_importfile(filename,i,met);
     end
 end
