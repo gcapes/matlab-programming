887bccf2e2fe2644c73b5a1e75aff8c06c0bf244
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-07-25 15:48:22 +0100

    Add 'clipping' debugging example

---
+++ debug/clipping.m
@@ -0,0 +1,26 @@
+%% Load data and calculate statistics
+load clippingdata.mat
+
+% F2_2
+ 
+F2_2N = mean(F2_2);
+F2_2M = mean(F2_2N,2);
+F2_2T = sum(F2_2);
+F2_2S = sum(F2_2T,2);
+F2_2X = nnz(F2_2);
+F2_2MMAX = max(F2_2);
+F2_2MAX = max(F2_2MMAX,[],2);
+
+% F2_3
+ 
+F2_3N = mean(F2_3);
+F2_3M = mean(F2_3N,2);
+F2_3T = sum(F2_3);
+F2_3S = sum(F2_3T,2);
+F2_3X = nnz(F2_3);
+F2_3MMAX = max(F2_3);
+F2_3MAX = max(F2_3MMAX,[],2);
+
+
+%% Make summary table
+A = double([F2_2M F2_2S F2_2X F2_2MAX ; F2_3M F2_3S F2_3X F2_3MAX]);
