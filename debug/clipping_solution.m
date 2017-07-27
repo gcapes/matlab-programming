a2f59e0ba30d786e3269f50e56a7cc74d89d8b12
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-07-27 15:44:18 +0100

    Add explanation for solution to 'clipping'

--- debug/clipping_solution.m
+++ debug/clipping_solution.m
@@ -1,6 +1,36 @@
 %% Load data and calculate statistics
 load clipping_data.mat
 
+% Explanation:
+% ------------
+% All the variables just loaded are unsigned 16-bit integers.
+% Some of the calculations below yield double precision results;
+% others yield integer results.
+
+% A matrix can only contain one data type, so if you try to put
+% numbers of a differing types into a matrix (e.g. uint16 and 
+% double, as we have here), MATLAB will automatically
+% convert some of the types.
+%
+% The original results matrix, A, has the class uint16.
+% The double precision numbers have been converted to unsigned
+% 16-bit integers.
+% The maximum value for this data type is intmax('uint16')
+% i.e. 2^16 -1 = 65535.
+% Larger values are simply assigned the maximum value this
+% number type can store, so they are clipped at 35535.
+
+% Solution:
+% ---------
+% Convert the integer variables to double before creating the 
+% summary matrix.
+%
+% Alternative solution:
+% If the summary values are to be used for further calculations,
+% it is useful to have them stored in a matrix.
+% If they are just for disply purposes, we could create a cell 
+% array instead using {}.
+
 % F2_2
  
 F2_2N = mean(F2_2);
