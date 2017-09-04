0488cc360ad07c0c6911ca418edb1965bd2e909d
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-09-04 15:30:24 +0100

    Improve solution to mono_increase

--- code/mono_increase.m
+++ code/mono_increase.m
@@ -4,7 +4,7 @@
 % I would argue that test 3 is wrong, and the uncommented code is
 % sufficient the other tests, which I agree with.
 function tf = mono_increase(x)
-    if isequal(sort(x),x) %&& x(1) ~= x(end) || length(x) == 1
+    if issorted(x) %&& x(1) ~= x(end) || length(x) == 1
         tf = true;
     else
         tf = false;
