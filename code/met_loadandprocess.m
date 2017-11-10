7325c7ef9af8085a25ecca1098db46fa290335fb
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-11-10 17:45:23 +0000

    Use camel case for met variables

--- code/met_loadandprocess.m
+++ code/met_loadandprocess.m
@@ -1,10 +1,10 @@
 % Load and process Met data
 
 % load raw meteorology data from 'data' subdirectory
-raw_met_data = met_loadfolder('data/met_mlo*.txt');
+rawMetData = met_loadfolder('data/met_mlo*.txt');
 
 % Combine columns within structure array
-met = combinestructcols(raw_met_data);
+met = combinestructcols(rawMetData);
 
 % Remove bad data points
 met.Temperature = badtonan(met.Temperature,-900);
