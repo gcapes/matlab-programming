7ef5cadf83aae0c10e3f5da34e1326f37de96d9f
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-11-13 11:31:24 +0000

    Correct indentation and spacing around declarations

--- code/ts_average.m
+++ code/ts_average.m
@@ -6,16 +6,16 @@
 % lfStart: low frequency start time stamp
 % lfEnd:   low frequency end time stamp
 function remapped = ts_average(hfStart,hfEnd,hfData,lfStart,lfEnd)
-
-lfDataLength=length(lfStart);
-% Preallocate array to contain results
-remapped=zeros(lfDataLength,1);
-    for i=1:lfDataLength
-        % Find data points which fall between lfStart and lfEnd
-        inRange=find(hfStart > lfStart(i) & hfEnd < lfEnd(i));
-        
-        % Average these data points to form new lf time series
-        % Exclude NaN values from result
-        remapped(i)=mean(hfData(inRange),'omitnan');
-    end
+	
+	lfDataLength = length(lfStart);
+	% Preallocate array to contain results
+	remapped = zeros(lfDataLength,1);
+	for i=1:lfDataLength
+		% Find data points which fall between lfStart and lfEnd
+		inRange = find(hfStart > lfStart(i) & hfEnd < lfEnd(i));
+		
+		% Average these data points to form new lf time series
+		% Exclude NaN values from result
+		remapped(i) = mean(hfData(inRange),'omitnan');
+	end
 end
